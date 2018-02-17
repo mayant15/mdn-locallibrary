a5931ecd010fa448e488b928186762c9a4bbea3d
Mayant Mukul <mayantmukul@hotmail.com>
2018-02-17 13:03:18 +0530

    Created login page

--- catalog/views.py
+++ catalog/views.py
@@ -9,18 +9,21 @@ def index(request):
     """
     # Generate counts of some of the main objects
     num_books=Book.objects.all().count()
-    num_instances=BookInstance.objects.all().count()
     # Available books (status = 'a')
-    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
+    num_books_available = 0
+    for book in Book.objects.all():
+        num_books_available = num_books_available + book.bookinstance_set.filter(status='a').count()
     num_authors=Author.objects.count()  # The 'all()' is implied by default.
-    num_sf=Genre.objects.filter(name__icontains='science').count()
-    num_ac=Book.objects.filter(author__first_name__icontains='agatha').count()
+
+    # Number of visits to this view, as counted in the session variable
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     # Render the HTML template index.html with the data in the context variable
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors,'num_sf':num_sf,'num_ac':num_ac},
+        context={'num_books':num_books,'num_books_available':num_books_available,'num_authors':num_authors,'num_visits':num_visits},
     )
 
 class BookListView(generic.ListView):
